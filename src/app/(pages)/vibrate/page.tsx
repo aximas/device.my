d743b6b195f756a4bf44ec67805f6f4411d933b3
aximas <northdakota778@gmail.com>
2023-03-07 23:56:33 +0500

    fix: vibrate function called

--- src/app/(pages)/vibrate/page.tsx
+++ src/app/(pages)/vibrate/page.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import {useState} from 'react'
 import {Button} from '@mui/material'
+import {vibrate} from '@/core/utils/helpers/vibrate'
 
 const Vibrate = () => {
     const milliSecondsData = [{name: '1 seconds', value: 1000}, {name: '5 seconds', value: 5000}]
 
-    const [ms, setMs] = useState(0);
-
     return <div>
         Vibrate for {milliSecondsData.map(s => {
-        return <Button onClick={() => setMs(s.value)} key={s.value}>{s.name}</Button>
+        return <Button onClick={() => vibrate(s.value)} key={s.value}>{s.name}</Button>
     })}
     </div>
 }
--- src/core/utils/helpers/vibrate.ts
+++ src/core/utils/helpers/vibrate.ts
@@ -1,4 +1,3 @@
 export const vibrate = (ms: number) => {
-    const getVibrateAPI = navigator.vibrate;
-    if (ms) getVibrateAPI(ms)
+    if (ms) navigator.vibrate(ms)
 }
